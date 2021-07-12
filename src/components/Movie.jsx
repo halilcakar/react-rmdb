3231a6e9b38276df3f7c19391a47a75d161fa088
halilcakar <hcakar.1992@gmail.com>
2021-07-12 18:54:51 +0300

    Create Actor component

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import { useParams } from 'react-router-dom'
 
 // Config
-import {} from '../config'
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config'
 
 // Components
-// import Grid from './Grid'
+import Grid from './Grid'
 import Spinner from './Spinner'
+import Actor from './Actor'
 import BreadCrumb from './BreadCrumb'
 import MovieInfo from './MovieInfo'
 import MovieInfoBar from './MovieInfoBar'
@@ -14,7 +15,7 @@ import MovieInfoBar from './MovieInfoBar'
 // Hooks
 import { useMovieFetch } from '../hooks/useMovieFetch'
 // Images
-// import NoImage from '../images/no_image.jpg'
+import NoImage from '../images/no_image.jpg'
 
 const Movie = () => {
   const { movieId } = useParams()
@@ -22,6 +23,7 @@ const Movie = () => {
   const { state: movie, loading, error } = useMovieFetch(movieId)
 
   if (error) return <div>Something went wrong...</div>
+
   if (loading) return <Spinner />
 
   return (
@@ -33,6 +35,21 @@ const Movie = () => {
         budget={movie.budget}
         revenue={movie.revenue}
       />
+
+      <Grid header="Actors">
+        {movie.actors.map((actor) => (
+          <Actor
+            key={actor.credit_id}
+            name={actor.name}
+            character={actor.character}
+            imageUrl={
+              actor.profile_path
+                ? `${IMAGE_BASE_URL}${POSTER_SIZE}${actor.profile_path}`
+                : NoImage
+            }
+          />
+        ))}
+      </Grid>
     </>
   )
 }
