071be5c75243b32b2c6d5c0585af15858720a529
halilcakar <hcakar.1992@gmail.com>
2021-07-12 12:45:32 +0300

    Global Styles added

--- src/App.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-const App = () => <div>Start here!</div>
-
-export default App;
---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { GlobalStyle } from './GlobalStyle'
+
+const App = () => (
+  <div className="App">
+    Start here!
+    <GlobalStyle />
+  </div>
+)
+
+export default App
