8a66a4997a6c44cf2b3f0e2a4736dcf2baad3ae0
halilcakar <hcakar.1992@gmail.com>
2021-07-12 16:15:39 +0300

    Mockup for Movie

--- src/components/Movie.jsx
+++ src/components/Movie.jsx
@@ -1,5 +1,25 @@
 import React from 'react'
 
-const Movie = () => <div>Movie</div>
+// Config
+import {} from '../config'
+
+// Components
+// import Grid from './Grid'
+// import Spinner from './Spinner'
+
+// Hooks
+
+// Images
+// import NoImage from '../images/no_image.jpg'
+
+const Movie = () => {
+  console.log('')
+
+  return (
+    <>
+      <div>Movie</div>
+    </>
+  )
+}
 
 export default Movie
