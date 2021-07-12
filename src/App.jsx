58050859ee7821bf40fa9b10d5776f606d2d9e9b
halilcakar <hcakar.1992@gmail.com>
2021-07-12 13:13:31 +0300

    Create Base Home component

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 // Components
 import Header from './components/Header'
+import Home from './components/Home'
 
 // Styles
 import { GlobalStyle } from './GlobalStyle'
@@ -9,7 +10,7 @@ import { GlobalStyle } from './GlobalStyle'
 const App = () => (
   <div className="App">
     <Header />
-    Start here!
+    <Home />
     <GlobalStyle />
   </div>
 )
