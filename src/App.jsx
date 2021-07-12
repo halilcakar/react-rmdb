bcae555e20593ca78991539f88208110f9497726
halilcakar <hcakar.1992@gmail.com>
2021-07-12 12:58:07 +0300

    Create Header Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+
+// Components
+import Header from './components/Header'
+
+// Styles
 import { GlobalStyle } from './GlobalStyle'
 
 const App = () => (
   <div className="App">
+    <Header />
     Start here!
     <GlobalStyle />
   </div>
