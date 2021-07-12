6ed0101c5a545849052d59094f97700bf7e5d8bb
halilcakar <hcakar.1992@gmail.com>
2021-07-12 13:25:47 +0300

    Use API to fetch movies

--- src/API.jsx
+++ src/API.jsx
@@ -38,29 +38,30 @@ const apiSettings = {
     const reqToken = await (await fetch(REQUEST_TOKEN_URL)).json()
     return reqToken.request_token
   },
-  authenticate: async (request_token, username, password) => {
+  authenticate: async (requestToken, username, password) => {
     const bodyData = {
       username,
       password,
-      request_token,
+      request_token: requestToken,
     }
-    // First authenticate the request_token
+    // First authenticate the requestToken
     const data = await (
       await fetch(LOGIN_URL, {
         ...defaultConfig,
         body: JSON.stringify(bodyData),
       })
     ).json()
-    // Then get the sessionId with the request_token
+    // Then get the sessionId with the requestToken
     if (data.success) {
       const sessionId = await (
         await fetch(SESSION_ID_URL, {
           ...defaultConfig,
-          body: JSON.stringify({ request_token }),
+          body: JSON.stringify({ request_token: requestToken }),
         })
       ).json()
       return sessionId
     }
+    return undefined
   },
   rateMovie: async (sessionId, movieId, value) => {
     const endpoint = `${API_URL}movie/${movieId}/rating?api_key=${API_KEY}&session_id=${sessionId}`
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,47 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+
+// API
+import API from '../API'
 
 // Config
-import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
+// import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config'
 
 // Components
 
 // Hooks
 
 // Image
-import NoImage from '../images/no_image.jpg'
+// import NoImage from '../images/no_image.jpg'
 
 const Home = () => {
   const [state, setState] = useState()
   const [loading, setLoading] = useState(false)
   const [error, setError] = useState(false)
+  console.log(state, loading, error)
+
+  const fetchMovies = async (page, searchTerm = '') => {
+    try {
+      setError(false)
+      setLoading(true)
+
+      const movies = await API.fetchMovies(searchTerm, page)
+
+      setState((prev) => ({
+        ...movies,
+        results:
+          page > 1 ? [...prev.results, ...movies.results] : [...movies.results],
+      }))
+    } catch (err) {
+      setError(true)
+    }
+
+    setLoading(false)
+  }
+
+  // Initial render
+  useEffect(() => {
+    fetchMovies(1)
+  }, [])
 
   return <div>Home Page</div>
 }
